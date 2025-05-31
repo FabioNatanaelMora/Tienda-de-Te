8b8ae187307c5ea20412fd74dfe38bd789dbae87
nicolas <nicolascontrerasporto@gmail.com>
2025-05-31 19:18:20 -0300

    se corrigieron enlaces y se eliminaron templates

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,41 +1,5 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const basePath = getBasePath();
-
-    fetch(`${basePath}templates/header.html`)
-        .then(res => {
-            if (!res.ok) throw new Error("No se pudo cargar el header");
-            return res.text();
-        })
-        .then(data => {
-            document.getElementById("header-placeholder").innerHTML = data;
-        })
-        .catch(e => console.error(e));
-
-    fetch(`${basePath}templates/footer.html`)
-        .then(res => {
-            if (!res.ok) throw new Error("No se pudo cargar el footer");
-            return res.text();
-        })
-        .then(data => {
-            document.getElementById("footer-placeholder").innerHTML = data;
-        })
-        .catch(e => console.error(e));
-
-    inicializarCarrusel();
-});
-
-function getBasePath() {
-    const path = window.location.pathname;
-    // Si la URL contiene /assets/pages/, subo un nivel
-    if (path.includes("/assets/pages/")) {
-        return "../";
-    }
-    // Si estamos directamente en /assets/ (index.html) no subo nada
-    return "";
-}
-
 function inicializarCarrusel() {
-    const slides = document.querySelectorAll('.carousel-slide');
+      const slides = document.querySelectorAll('.carousel-slide');
     const prevBtn = document.querySelector('.carousel-btn.prev');
     const nextBtn = document.querySelector('.carousel-btn.next');
 
